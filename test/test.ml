2ac7b89505f3ccfc897e58a6e484067cf6095d5f
Scott Meeuwsen <scott@apisw.com>
2020-09-19 23:38:35 -0400

    Import basic tests

---
+++ test/test.ml
@@ -0,0 +1,98 @@
+open OUnit2
+open Parsing
+
+let code_func_bind =
+  {src|
+func test(h, w: double):
+end
+
+func test2(h: int) -> int:
+    1
+end
+|src}
+
+let code_simple =
+  {src|
+func circle(r: double) -> double:
+    3.14 * r * r
+end
+
+# A comment
+func cyl(r: double, h: double) -> double:
+    h * circle(r) # Another comment
+end
+
+# NOTE: In real lang main takes list of args and returns int
+func main():
+    let vol: double = cyl(2.0, 5.0)
+    println(vol)
+end
+|src}
+
+let expect_func_bind =
+  {src|Program
+└──Function: test
+   └──Arg: h
+      └──Type: double
+   └──Arg: w
+      └──Type: double
+   └──Return Type: unit
+   └──Block: 
+└──Function: test2
+   └──Arg: h
+      └──Type: int
+   └──Return Type: int
+   └──Block: 
+      └──Integer: 1
+|src}
+
+let expect_simple =
+  {src|Program
+└──Function: circle
+   └──Arg: r
+      └──Type: double
+   └──Return Type: double
+   └──Block: 
+      └──Binop: *
+         └──Binop: *
+            └──Double: 3.140000
+            └──Ident: r
+         └──Ident: r
+└──Function: cyl
+   └──Arg: r
+      └──Type: double
+   └──Arg: h
+      └──Type: double
+   └──Return Type: double
+   └──Block: 
+      └──Binop: *
+         └──Ident: h
+         └──Call: circle
+            └──Ident: r
+└──Function: main
+   └──Return Type: unit
+   └──Block: 
+      └──Let: vol
+         └──Type: double
+         └──Call: cyl
+            └──Double: 2.000000
+            └──Double: 5.000000
+      └──Call: println
+         └──Ident: vol
+|src}
+
+let test_ast expected code _ =
+  let lexbuf = Parse.lexbuf_from_string code in
+  let ast = Parse.gen_ast_string lexbuf in
+  let diffs = Odiff.strings_diffs expected ast in
+  print_endline (Odiff.string_of_diffs diffs);
+  assert_equal ~msg:"Expected ast not equal to actual" expected ast
+
+let tests =
+  "ast tests"
+  >::: [
+         "simple ast" >:: test_ast expect_simple code_simple;
+         "func bind test" >:: test_ast expect_func_bind code_func_bind;
+       ]
+
+let () = run_test_tt_main tests
