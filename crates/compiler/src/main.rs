625a153ea9d9eebd2ea774fd91c7a3b0d4e104a8
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2023-10-01 16:10:14 -0400

    Initial commit of Rust compiler. Just a "hello world" of sorts for an indention based syntax

---
+++ crates/compiler/src/main.rs
@@ -0,0 +1,15 @@
+use pest::Parser;
+use pest_derive::Parser;
+use std::{env, fs};
+
+#[derive(Parser)]
+#[grammar = "../../interop.pest"]
+pub struct InteropParser;
+
+fn main() {
+    let filename = env::args().nth(1).unwrap();
+    let src = fs::read_to_string(filename).unwrap();
+    let parse = InteropParser::parse(Rule::Grammar, &src);
+
+    println!("{:#?}", parse);
+}
