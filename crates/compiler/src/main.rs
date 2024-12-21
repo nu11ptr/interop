553b48c171e9de031a790c812a494a036ae54164
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2024-12-21 09:33:05 -0500

    Refactor parser grammar naming syntax

--- crates/compiler/src/main.rs
+++ crates/compiler/src/main.rs
@@ -9,7 +9,7 @@ pub struct InteropParser;
 fn main() {
     let filename = env::args().nth(1).unwrap();
     let src = fs::read_to_string(filename).unwrap();
-    let parse = InteropParser::parse(Rule::Grammar, &src);
+    let parse = InteropParser::parse(Rule::grammar, &src);
 
     println!("{:#?}", parse);
 }
